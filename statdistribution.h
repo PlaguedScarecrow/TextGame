cb18ac5a72e2e24d4049f458651b46b8b92bc719
Azrael <36523191+PlaguedScarecrow@users.noreply.github.com>
2022-05-16 18:17:15 -0400

    Update statdistribution.h

--- statdistribution.h
+++ statdistribution.h
@@ -1,14 +1,30 @@
 #pragma once
-
 //#include "creature.h"
 #include "dice.h"
-#include "multirolls.h"
-
-#include <stdlib.h>
+#include "multiroll.h"
+#include "startingarray.h"
 #include <iostream>
 
 
-int StartingAttributes()
+template <typename A>
+inline A Value0(A a, A b, A c, A d, A e, A f, A g)
+{
+	return a * (a == b || a == c || a == d || a == e || a == f || a == g);
+}
+
+template <typename B>
+inline B Value1(B a, B b, B c, B d, B e, B f)
+{
+	return a * (a == b || a == c || a == d || a == e || a == f);
+}
+
+template <typename C>
+inline C Value2(C a, C b, C c, C d, C e)
+{
+	return a * (a == b || a == c || a == d || a == e);
+}
+
+int main()
 {
 	int strength;
 	int dexterity;
@@ -16,20 +32,134 @@ int StartingAttributes()
 	int intelligence;
 	int wisdom;
 	int charisma;
-	
-	printf("Welcome to Azrael's Dungeons and Dragons Text Game. Currently, your first step will be to assign numbers to your "); 
-	printf("attributes in order to determine your strengths and weaknesses. Plese press enter to begin rolling for your attributes.");
-	
 
-	printf("Please enter the amount of points you wish to put into your strength:");
+	int statValue0 = StartingRolls();
+	int statValue1 = StartingRolls();
+	int statValue2 = StartingRolls();
+	int statValue3 = StartingRolls();
+	int statValue4 = StartingRolls();
+	int statValue5 = StartingRolls();
+
+	printf("Your rolled stats are: ");
+	std::cout << statValue0 << ", ";
+	std::cout << statValue1 << ", ";
+	std::cout << statValue2 << ", ";
+	std::cout << statValue3 << ", ";
+	std::cout << statValue4 << ", ";
+	std::cout << statValue5 << "." << std::endl;
+
+	std::vector<int> Array = { statValue0, statValue1, statValue2, statValue3, statValue4, statValue5 };
+
+	system("pause");
+
+
+	printf("Please enter the value you wish to put into your strength: ");
 	std::cin >> strength;
-	printf("Please enter the amount of points you wish to put into your dexterity:");
+	int testValue0 = Value0(strength, Array.at(0), Array.at(1), Array.at(2), Array.at(3), Array.at(4), Array.at(5));
+
+	if (testValue0 == 0)
+	{
+		printf("You did not enter a valid input!");
+	}
+	else
+	{
+		printf("Your strength is a "); std::cout << strength; printf("!");
+
+		if (strength == Array.at(0))
+		{
+			Array.erase(Array.begin());
+		}
+		else if (strength == Array.at(1))
+		{
+			Array.erase(Array.begin() + 1);
+		}
+		else if (strength == Array.at(2))
+		{
+			Array.erase(Array.begin() + 2);
+		}
+		else if (strength == Array.at(3))
+		{
+			Array.erase(Array.begin() + 3);
+		}
+		else if (strength == Array.at(4))
+		{
+			Array.erase(Array.begin() + 4);
+		}
+		else
+		{
+			Array.erase(Array.begin() + 5);
+		}
+	}
+
+	printf("Please enter the value you wish to put into your dexterity:");
 	std::cin >> dexterity;
-	printf("Please enter the amount of points you wish to put into your constitution:");
+
+	int testValue1 = Value1(dexterity, Array.at(0), Array.at(1), Array.at(2), Array.at(3), Array.at(4));
+
+	if (testValue1 == 0)
+	{
+		printf("You did not enter a valid input!");
+	}
+	else
+	{
+		printf("Your dexterity is a "); std::cout << dexterity; printf("!");
+
+		if (dexterity == Array.at(0))
+		{
+			Array.erase(Array.begin());
+		}
+		else if (dexterity == Array.at(1))
+		{
+			Array.erase(Array.begin() + 1);
+		}
+		else if (dexterity == Array.at(2))
+		{
+			Array.erase(Array.begin() + 2);
+		}
+		else if (dexterity == Array.at(3))
+		{
+			Array.erase(Array.begin() + 3);
+		}
+		else
+		{
+			Array.erase(Array.begin() + 4);
+		}
+	}
+
+	printf("Please enter the value you wish to put into your constitution:");
 	std::cin >> constitution;
-	printf("Please enter the amount of points you wish to put into your intelligence:");
+
+	int testValue2 = Value2(constitution, Array.at(0), Array.at(1), Array.at(2), Array.at(3));
+
+	if (testValue2 == 0)
+	{
+		printf("You did not enter a valid input!");
+	}
+	else
+	{
+		printf("Your constitution is a "); std::cout << constitution; printf("!");
+
+		if (constitution == Array.at(0))
+		{
+			Array.erase(Array.begin());
+		}
+		else if (constitution == Array.at(1))
+		{
+			Array.erase(Array.begin() + 1);
+		}
+		else if (constitution == Array.at(2))
+		{
+			Array.erase(Array.begin() + 2);
+		}
+		else
+		{
+			Array.erase(Array.begin() + 3);
+		}
+	}
+
+	printf("Please enter the value you wish to put into your intelligence:");
 	std::cin >> intelligence;
-	printf("Please enter the amount of points you wish to put into your wisdom:");
+	printf("Please enter the value you wish to put into your wisdom:");
 	std::cin >> wisdom;
 	printf("Please enter the amount of points you wish to put into your charisma:");
 	std::cin >> charisma;
